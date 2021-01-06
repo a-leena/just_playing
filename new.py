a4e1eef31379b37c3edea3f235586b4bdeb26f9b
Aleena <aleenabiju09@gmail.com>
2021-01-07 00:01:11 +0530

    Just made a teeny tiny change.

--- new.py
+++ new.py
@@ -1,4 +1,4 @@
-print("Hello there!")
+print("Hello there! This repo is just for playing with GIT and GITHUB. Have fun!!!")
 def intro (a,b):
     return "Hey!! My name is {} and I'm here {}.".format(a,b)
-print(intro(input("Name: "), input("Purpose: "))) 
+print(intro(input("Name: "), input("Purpose: ")))
